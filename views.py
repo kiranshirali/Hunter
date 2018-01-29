44648af69e7227314550c58a8253d5e832c15861
kiranshirali <kiranshirali@gmail.com>
2018-01-29 01:27:35 -0800

    Moving certain files around for better efficiency

--- views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.template.response import TemplateResponse
-from django.views.generic.base import TemplateView
-
-def index(request): 
-    return render(request, 'hunter.html', {})
-    
